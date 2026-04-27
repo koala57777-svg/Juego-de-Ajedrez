2f3b751471b27c478ab153689bfb50683a32a5b4
Samir1984 <allandanielito1984@gmail.com>
2026-04-26 23:28:50 -0600

    v0.1- Implementación de Login con intentos , validación, acceso al sistema, y credenciales programadas.

--- Juego-Ajedrez/Program.cs
+++ Juego-Ajedrez/Program.cs
@@ -1,13 +1,38 @@
-﻿string contraseña = "";
-string usuario = "";
+﻿string contraseña = "E$quizo1984";
+string usuario = "Koalas";
+string usuarioo = "";
 int intentos= 3;
-do {
-    Console.WriteLine("Por favor ingrese el usuario");
-    for (int i = 0; i<3; i++)
-    {
-        Console.WriteLine($"Intento {i + 1}");
-        usuario= Console.ReadLine();
-        intentos--;
-    }
+string contraseñaa = "";
+bool login=false;
 
-}while (intentos>0);
+for (int i = 0; i < 3; i++)
+{ 
+    Console.Write($"Por favor ingrese el usuario, intento {intentos}: ");
+    usuarioo = Console.ReadLine();
+    Console.Write($"Por favor, ingrese la contraseña, intento {intentos}: ");
+    contraseñaa = Console.ReadLine();
+    Console.WriteLine();
+    if (usuarioo == usuario && contraseñaa == contraseña)
+    {
+        login = true; break;
+    }
+    else
+    {
+        intentos--;
+        Console.Write($"Datos incorrectos. Te quedan {intentos} intento(s).");
+        Console.ReadKey();
+        Console.Clear();
+    }
+ 
+}
+Console.Clear();
+if (login == true)
+{
+    Console.WriteLine("El usuario ingresado y la contraseña ingresada son correctos.\n¡Bienvenido!");
+}
+else if (login == false)
+{
+
+    Console.WriteLine("Intentos acabados. Acceso denegado.");
+    Console.ReadKey(); return;
+}
