e302fcc916c04bc21d0614239876bc49b5bcab43
Samir1984 <allandanielito1984@gmail.com>
2026-04-26 22:28:08 -0600

    Intento de login

--- Juego-Ajedrez/Program.cs
+++ Juego-Ajedrez/Program.cs
@@ -1,3 +1,13 @@
-﻿Console.WriteLine("Ingrese la contraseña:");
+﻿string contraseña = "";
+string usuario = "";
+int intentos= 3;
+do {
+    Console.WriteLine("Por favor ingrese el usuario");
+    for (int i = 0; i<3; i++)
+    {
+        Console.WriteLine($"Intento {i + 1}");
+        usuario= Console.ReadLine();
+        intentos--;
+    }
 
-double contraseña = double.Parse(Console.ReadLine());
+}while (intentos>0);
